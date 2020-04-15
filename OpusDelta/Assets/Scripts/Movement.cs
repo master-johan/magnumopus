4254d563b3ba60ab3b5836747e10ca27e0151bf2
kallesongotto <kallesongotto@gmail.com>
2020-04-15 14:38:53 +0200

    TEST

--- OpusDelta/Assets/Scripts/Movement.cs
+++ OpusDelta/Assets/Scripts/Movement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+<<<<<<< HEAD
 
     public Rigidbody rb;
+=======
+>>>>>>> 9b569761e1c7877e5c19d8328e768667d896932f
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,10 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
   
+=======
+        
+>>>>>>> 9b569761e1c7877e5c19d8328e768667d896932f
     }
 }
