a7cccd263f64b78a84bea5d212f9e9e7f0a885a9
Johan Fritiofsson <fritiofsson@gmail.com>
2020-05-01 16:41:26 +0200

    Player object, basic animation

---
+++ OpusDelta/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /*[SerializeField]*/ private Animator animator;
+    private CharacterController cc;
+
+    Vector2 direction = new Vector2();
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        animator.updateMode = AnimatorUpdateMode.UnscaledTime;
+    }
+
+    void Update()
+    {
+
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        direction.Normalize();
+
+        animator.SetFloat("DirX", direction.x);
+        animator.SetFloat("DirY", direction.y);
+        animator.SetBool("Moving", direction.sqrMagnitude > 0);
+
+        if (direction.sqrMagnitude > 0)
+        {
+            Vector3 movement = new Vector3(direction.x, 0, direction.y);
+            cc.Move(movement * 2 * Time.deltaTime);
+        }
+
+    }
+}
