fd59b34b6feb55c19fa0ca95e8ea84470f8791c3
kallesongotto <kallesongotto@gmail.com>
2020-05-07 11:48:51 +0200

    Otto
    Main menu och enemy.

---
+++ OpusDelta/Assets/FSM/Scripts/Enemy_Chase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Chase : StateMachineBehaviour
+{
+    Transform player;
+    Rigidbody rb;
+    public float speed;
+    public float attackRange;
+    public float attentionRange;
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.gameObject.GetComponent<Rigidbody>();
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 target = new Vector3(player.position.x, rb.position.y, player.position.z);
+        Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.deltaTime);
+        Vector3 forwards = target - rb.transform.position;
+        rb.MovePosition(newPos);
+
+        if (Vector3.Distance(rb.transform.position, player.transform.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
+        if (Vector3.Distance(rb.transform.position, player.transform.position) >= attentionRange)
+        {
+            animator.SetTrigger("Walk");
+        }
+
+        rb.transform.forward = forwards;
+
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack");
+        animator.ResetTrigger("Walk");
+    }
+
+
+}
---
+++ OpusDelta/Assets/FSM/Scripts/Enemy_Idle.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Idle : StateMachineBehaviour
+{
+
+    Transform player;
+    Rigidbody rb;
+
+    Vector3 entryPos;
+    Vector3[] wanderPoints;
+    Vector3 wanderPos;
+    Vector3 velocity;
+
+    public float runningRange;
+    public float speed;
+   
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.gameObject.GetComponent<Rigidbody>();
+        wanderPoints = new Vector3[4];
+        wanderPos = Vector3.zero;
+        velocity = Vector3.zero;
+
+        entryPos = rb.position;
+
+        CreatingWanderPoints();
+        GetRandomWanderPos();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+     
+
+        if (Vector3.Distance(wanderPos, rb.position) <= 1)
+        {
+            GetRandomWanderPos();
+        }
+
+        velocity = wanderPos.normalized * speed * Time.deltaTime;
+
+        rb.MovePosition(Vector3.MoveTowards(rb.position, wanderPos, speed * Time.deltaTime));
+
+        //rb.MovePosition(Vector3.SmoothDamp(rb.position, wanderPos, ref velocity, 1));
+        
+        rb.transform.forward = wanderPos - rb.position;
+
+        if (Vector3.Distance(player.transform.position, rb.transform.position) <= runningRange)
+        {
+            animator.SetTrigger("Run");
+        }
+    }
+
+    private void CreatingWanderPoints()
+    {
+        Vector3 offsetRight = new Vector3(entryPos.x + 5, entryPos.y, entryPos.z);
+        Vector3 offsetLeft = new Vector3(entryPos.x - 5, entryPos.y, entryPos.z);
+        Vector3 offsetForward = new Vector3(entryPos.x, entryPos.y, entryPos.z + 5);
+        Vector3 offsetBackward = new Vector3(entryPos.x, entryPos.y, entryPos.z - 5);
+
+        wanderPoints[0] = offsetRight;
+        wanderPoints[1] = offsetLeft;
+        wanderPoints[2] = offsetForward;
+        wanderPoints[3] = offsetBackward;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
+    {
+        animator.ResetTrigger("Run");
+    }
+
+    private void GetRandomWanderPos()
+    {
+        wanderPos = wanderPoints[Random.Range(0, wanderPoints.Length)];
+    }
+
+}
