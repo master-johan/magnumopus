9dff0bdb10fd7359baa48e86b18a94300a3e1258
Johan Fritiofsson <fritiofsson@gmail.com>
2020-05-08 10:42:14 +0200

    Remove default assests

--- OpusDelta/Assets/Scripts/PlayerController.cs
+++ OpusDelta/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     /*[SerializeField]*/ private Animator animator;
+    [SerializeField] private BoxCollider feetCollider;
     private CharacterController cc;
+    private bool isGrounded;
+    float gravity;
 
     Vector2 direction = new Vector2();
     private void Start()
@@ -27,11 +31,28 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("DirY", direction.y);
         animator.SetBool("Moving", direction.sqrMagnitude > 0);
 
+        if (isGrounded)
+        {
+            gravity = 0;
+        }
+        else
+        {
+            gravity = -9.82f * Time.deltaTime;
+        }
+
+
+
         if (direction.sqrMagnitude > 0)
         {
-            Vector3 movement = new Vector3(direction.x, 0, direction.y);
+            Vector3 movement = new Vector3(direction.x, gravity, direction.y);
             cc.Move(movement * 2 * Time.deltaTime);
         }
 
+        CheckIfGrounded();
+    }
+
+    private bool CheckIfGrounded()
+    {
+        throw new NotImplementedException();
     }
 }
