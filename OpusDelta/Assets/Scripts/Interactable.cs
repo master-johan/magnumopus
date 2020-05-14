cf646fc51d317d51e7fdefbec278f7909d8a005c
kallesongotto <kallesongotto@gmail.com>
2020-05-14 21:47:18 +0200

    OttoJeppa Jobb
    - Slag
    - Inventory

--- OpusDelta/Assets/Scripts/Interactable.cs
+++ OpusDelta/Assets/Scripts/Interactable.cs
@@ -16,14 +16,22 @@ public class Interactable : MonoBehaviour
     {
         //This methos is meant to be overwritten
     }
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     void Update()
     {
-        if (isFocused && !hasInteracted)
+        if (!hasInteracted)
         {
             float distance = Vector3.Distance(player.position, transform.position);
+
+
             if (distance <= radius)
             {
+
+
                 Interact();
                 hasInteracted = true;
             }
