203ceb7167fd01057f6129b1871cc8fd1943b6ca
axlhammer <48758283+axlhammer@users.noreply.github.com>
2020-05-14 21:48:02 +0200

    Healthbar
    -Healthbar UI
    -Health script on player

---
+++ OpusDelta/Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+    private int currentHealth;
+
+    public event Action<float> healthPctChanged = delegate { };
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ModifyHealth(int amount)
+    {
+        currentHealth += amount;
+        float currentHeathPct = (float)currentHealth / (float)maxHealth;
+        healthPctChanged(currentHeathPct);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ModifyHealth(-10);
+        }
+    }
+}
