4c5064dbb7596ce4e46236668847280229ac3c1f
JeppaVansinnig <Jesper.larsson1999@gmail.com>
2020-05-14 21:46:39 +0200

    potion

---
+++ OpusDelta/Assets/Stats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float currentHealth { get; private set; }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        PotionsManager.instance.onPotionsUsed += OnPotionsUsed;
+    }
+
+    void OnPotionsUsed()
+    {
+        currentHealth += 5;
+        Debug.Log(currentHealth);
+    }
+}
